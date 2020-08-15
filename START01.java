348f3e1638bc6187b3e402d3a95f3a3f65ef280f
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2020-08-15 13:30:42 +0530

    Create START01.java

---
+++ START01.java
@@ -0,0 +1,17 @@
+/*
+ * Title   : Number Mirror
+ * Time    : 0.07 sec
+ * Status  : ACCEPTED
+ * Problem : https://www.codechef.com/problems/START01
+ */
+ 
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+        sc.close();
+    }
+}
